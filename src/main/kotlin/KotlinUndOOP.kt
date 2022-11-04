3728a38cbc902fb7bbf4f204ef2475d3bd6484a9
M7Development <mateusz.munacoha@gmail.com>
2022-11-05 00:27:40 +0100

    OOP+get & set

--- src/main/kotlin/KotlinUndOOP.kt
+++ src/main/kotlin/KotlinUndOOP.kt
@@ -1,5 +1,31 @@
+import kotlin.math.*
 
+//_________________________________________________________________Klasse mit zwei Funktionen mit GETTER & SETTER
+class Kreis(
+    radius :Double){
+   public var radius :Double=radius
+       get() = field                        //__________________Getter
+       set(value) {                         //__________________Setter
+           if (value>=0.0){
+               field = value
+           }
+       }
+    public var isValidCircle: Boolean = radius >=0.0
+        get() = radius >= 0.0
+
+    fun umpfangBerechnen():Double{
+        var umpfang:Double
+        umpfang=radius*2*PI
+        return umpfang
+    }
+    fun areaBerechnen():Double = radius * radius * PI
+}
 
 fun main(args: Array<String>) {
 
+    var k: Kreis = Kreis(5.0)
+    k.radius = -10.0
+    println(k.isValidCircle)
+
+println("Umpfang ist: ${k.umpfangBerechnen()} und die Fläche ist: ${k.areaBerechnen()}")
 }
